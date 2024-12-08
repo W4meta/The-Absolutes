9e85d5ae50e9b6ad7c13efb228c621ca3ab893ca
W4meta <suportgmtk@gmail.com>
2024-12-08 12:22:59 +0700

    asdaw

--- js/index.js
+++ js/index.js
@@ -14,46 +14,46 @@ menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const slider = document.querySelector('.bentobox');
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+// document.addEventListener('DOMContentLoaded', () => {
+//     const slider = document.querySelector('.bentobox');
+//     let isDown = false;
+//     let startX;
+//     let scrollLeft;
 
-    slider.addEventListener('mousedown', (e) => {
-        isDown = true;
-        slider.style.cursor = 'grabbing';
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
+//     slider.addEventListener('mousedown', (e) => {
+//         isDown = true;
+//         slider.style.cursor = 'grabbing';
+//         startX = e.pageX - slider.offsetLeft;
+//         scrollLeft = slider.scrollLeft;
+//     });
 
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-        slider.style.cursor = 'grab';
-    });
+//     slider.addEventListener('mouseleave', () => {
+//         isDown = false;
+//         slider.style.cursor = 'grab';
+//     });
 
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-        slider.style.cursor = 'grab';
-    });
+//     slider.addEventListener('mouseup', () => {
+//         isDown = false;
+//         slider.style.cursor = 'grab';
+//     });
 
-    slider.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = (x - startX) * 2;
-        slider.scrollLeft = scrollLeft - walk;
-    });
+//     slider.addEventListener('mousemove', (e) => {
+//         if (!isDown) return;
+//         e.preventDefault();
+//         const x = e.pageX - slider.offsetLeft;
+//         const walk = (x - startX) * 2;
+//         slider.scrollLeft = scrollLeft - walk;
+//     });
 
-    slider.addEventListener('touchstart', (e) => {
-        startX = e.touches[0].pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
+//     slider.addEventListener('touchstart', (e) => {
+//         startX = e.touches[0].pageX - slider.offsetLeft;
+//         scrollLeft = slider.scrollLeft;
+//     });
 
-    slider.addEventListener('touchmove', (e) => {
-        if (e.touches.length !== 1) return;
-        const x = e.touches[0].pageX - slider.offsetLeft;
-        const walk = (startX - x) * 2;
-        slider.scrollLeft = scrollLeft + walk;
-    });
-});
+//     slider.addEventListener('touchmove', (e) => {
+//         if (e.touches.length !== 1) return;
+//         const x = e.touches[0].pageX - slider.offsetLeft;
+//         const walk = (startX - x) * 2;
+//         slider.scrollLeft = scrollLeft + walk;
+//     });
+// });
